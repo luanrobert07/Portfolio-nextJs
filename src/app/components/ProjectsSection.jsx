69ad0061921ee43df1c7c5c6d80f6062c535924d
luanrobert07 <luan.robert@gec.inatel.br>
2025-02-13 18:57:07 +0100

    feat: update the project image

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -9,7 +9,7 @@ const projectsData = [
     "id": 1,
     "title": "Bullvest",
     "description": "This is a mobile application with React Native and Node.js about investment.",
-    "image": "/images/projects/1.png",
+    "image": "/images/projects/bullvest.png",
     "tag": ["All", "Mobile"],
     "gitUrl": "/",
     "previewUrl": "/"
@@ -18,7 +18,7 @@ const projectsData = [
     "id": 2,
     "title": "To-do list",
     "description": "A simple and efficient to-do list application built with React Native and Node.js to help users organize their daily tasks.",
-    "image": "/images/projects/2.png",
+    "image": "/images/projects/to_do_list.png",
     "tag": ["All", "Mobile"],
     "gitUrl": "/",
     "previewUrl": "/"
@@ -27,7 +27,7 @@ const projectsData = [
     "id": 3,
     "title": "GymPass App",
     "description": "A web platform developed with React and Node.js to manage gym memberships, bookings, and workouts.",
-    "image": "/images/projects/3.png",
+    "image": "/images/projects/gym.png",
     "tag": ["All", "Web"],
     "gitUrl": "/",
     "previewUrl": "/"
@@ -36,7 +36,7 @@ const projectsData = [
     "id": 4,
     "title": "Mountain Events",
     "description": "A web application for discovering and managing outdoor events, built with React and Node.js.",
-    "image": "/images/projects/4.png",
+    "image": "/images/projects/montain_events.png",
     "tag": ["All", "Web"],
     "gitUrl": "/",
     "previewUrl": "/"
@@ -45,7 +45,7 @@ const projectsData = [
     "id": 5,
     "title": "Gym plus plus",
     "description": "A mobile fitness application developed with React Native and Node.js to help users track workouts, progress, and nutrition.",
-    "image": "/images/projects/5.png",
+    "image": "/images/projects/gym_plus_plus.png",
     "tag": ["All", "Mobile"],
     "gitUrl": "/",
     "previewUrl": "/"
@@ -54,7 +54,7 @@ const projectsData = [
     "id": 6,
     "title": "Maternar App",
     "description": "A mobile application designed to provide guidance and support for mothers during pregnancy and early childhood, built with React Native and Node.js.",
-    "image": "/images/projects/6.png",
+    "image": "/images/projects/maternar.png",
     "tag": ["All", "Mobile"],
     "gitUrl": "/",
     "previewUrl": "/"
